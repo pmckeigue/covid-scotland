b7f66cbec272b53d32976d9ce743f5c8fa1ae669
Amanda Weir <amandw02@nssrstudio02.csa.scot.nhs.uk>
2020-06-24 12:10:19 +0100

    Includes recodes for disaggregated smr/onomap variables for Ethnicity Report

--- ethnic_assign.R
+++ ethnic_assign.R
@@ -104,3 +104,99 @@ collapseto5.onomap.group <- function(onomap.group) {
     ethnic5 <- relevel(as.factor(ethnic5), ref="White")
     return(ethnic5)
 }
+
+### DISSAGGREGATED RECODES FOR ETHNICITY REPORT
+
+## extra codes: 1[DEFGHJ] White, 3[ABC] South Asian, 3[E] Chinese, 3D Other Asian, 4[ABCE]  African
+
+collapseto9.ethnicsmr <- function(ethnic.smr) {
+  ethnic9.smr <- character(length(ethnic.smr))
+  ethnic9.smr[grep("^1[A-Z]$", ethnic.smr)] <- "White"
+  ethnic9.smr[grep("^1[L]$", ethnic.smr)] <- "White Polish"
+  ethnic9.smr[grep("^2[A-Z]$", ethnic.smr)] <- "Other"
+  ethnic9.smr[grep("^3[BCFH]$", ethnic.smr)] <- "Pakistani/Bangladeshi"
+  ethnic9.smr[grep("^3[AG]$", ethnic.smr)] <- "Indian"
+  ethnic9.smr[grep("^3[EJ]$", ethnic.smr)] <- "Chinese"
+  ethnic9.smr[grep("^3[DZ]$", ethnic.smr)] <- "Other"
+  ethnic9.smr[grep("^4[AE]$", ethnic.smr)] <- "Caribbean"
+  ethnic9.smr[grep("^4[BDY]$", ethnic.smr)] <- "African"
+  ethnic9.smr[grep("^4[C]$", ethnic.smr)] <- "Black"
+  #ethnic9.smr[grep("^4[F]$", ethnic.smr)] <- "Black" #4F not included in coding above
+  ethnic9.smr[grep("^5[C]$", ethnic.smr)] <- "Caribbean"
+  ethnic9.smr[grep("^5[ABDY]$", ethnic.smr)] <- "Black"
+  #ethnic9.smr[grep("^4[Z]$", ethnic.smr)] <- "Other" #not included in coding above
+  ethnic9.smr[grep("^6[AZ]$", ethnic.smr)] <- "Other"
+  ethnic9.smr[grep("^9", ethnic.smr)] <- NA
+  ethnic9.smr[ethnic9.smr==""] <- NA
+  ethnic9.smr <- as.factor(ethnic9.smr)
+  ethnic9.smr <- factor(ethnic9.smr, levels=levels(ethnic9.smr)[c(8,9,3,1,2,4,7,5,6)])
+  return(ethnic9.smr)
+}
+
+group.onomap2 <- function(OnolyticsType, GeographicalArea) {    
+  OnolyticsType <- car::recode(OnolyticsType,
+                               "'NOT FOUND'=NA; 'INTERNATIONAL'=NA; 'UNCLASSIFIED'=NA; 'VOID'=NA; 'VOID - FORENAME'=NA; 'VOID INITIAL'=NA")
+  
+  table(OnolyticsType[GeographicalArea=="SOUTH ASIA"])
+  table(OnolyticsType[GeographicalArea=="AFRICA"])
+  table(OnolyticsType[GeographicalArea=="BRITISH ISLES"])
+  table(OnolyticsType[GeographicalArea=="EAST ASIA"])
+  table(OnolyticsType[GeographicalArea=="MIDDLE EAST"])
+  
+  eth8 <- rep("Other", length(OnolyticsType))
+  eth8[is.na(OnolyticsType)] <- NA
+  eth8[OnolyticsType=="CHINESE" |
+         OnolyticsType=="HONG KONGESE" |
+         OnolyticsType=="SINGAPORESE"] <- "Chinese"
+  eth8[GeographicalArea=="EAST ASIA" &
+         eth8 != "Chinese"] <- "Other Asia & Pacific"
+  
+  eth8[GeographicalArea=="SOUTH ASIA"] <- "Other South Asian"
+  
+  eth8[GeographicalArea=="BRITISH ISLES"] <- "Britain&Ireland"
+  eth8[GeographicalArea=="CENTRAL EUROPE" |
+         GeographicalArea=="EASTERN EUROPE" |
+         GeographicalArea=="NORTHERN EUROPE" |
+         GeographicalArea=="SOUTHERN EUROPE" |
+         OnolyticsType=="AFRIKAANS"] <- "Other Europe"
+  
+  eth8[GeographicalArea=="AFRICA" &
+         OnolyticsType != "AFRIKAANS" &
+         OnolyticsType != "LIBYAN"] <- "Black African"
+  eth8[GeographicalArea=="MIDDLE EAST" &
+         OnolyticsType != "MUSLIM"] <- "East Med"
+  eth8[OnolyticsType == "MUSLIM"] <- "South Asian" # "Muslim, not localized"
+  
+  eth8[OnolyticsType == "BLACK CARIBBEAN"] <- "Black Caribbean"
+  
+  eth8[OnolyticsType == "BANGLADESHI"] <- "Muslim South Asian"
+  eth8[OnolyticsType == "MUSLIM INDIAN"] <- "Muslim South Asian"
+  eth8[OnolyticsType == "PAKISTANI"] <- "Muslim South Asian"
+  eth8[OnolyticsType == "PAKISTANI KASHMIR"] <- "Muslim South Asian"
+  eth8[OnolyticsType == "MUSLIM"] <- "Muslim South Asian"
+  
+  eth8[OnolyticsType == "POLISH"] <- "White Polish"
+  return(eth8)
+}
+
+collapseto8.onomap.group <- function(onomap.group) {
+  ## reduce to 5 categories: White, South Asian, Chinese, Black, Other
+  ethnic8 <- car::recode(onomap.group, "'Britain&Ireland'='White';  'Other Europe'='White';  'East Med'='Other'; 'Other Asia & Pacific'='Other'") 
+  ethnic8 <- as.factor(ethnic8)
+  ethnic8 <- factor(ethnic8, levels=levels(ethnic8)[c(7,8,2,1,3,4,6,5)])
+  ethnic8 <- relevel(as.factor(ethnic8), ref="White")
+  return(ethnic8)
+}
+
+collapseto7.onomap.group <- function(onomap.group) {
+  ## reduce to 5 categories: White, South Asian, Chinese, Black, Other
+  ethnic8 <- car::recode(onomap.group, "'Britain&Ireland'='White';  'Other Europe'='White';  'East Med'='Other'; 'Other Asia & Pacific'='Other'") 
+  ethnic7 <- as.character(ethnic8)
+  ethnic7 <- car::recode(ethnic7, "'Black Caribbean'='Black'")
+  ethnic7 <- car::recode(ethnic7, "'Black African'='Black'")
+  ethnic7 <- as.factor(ethnic7)
+  ethnic7 <- factor(ethnic7, levels=levels(ethnic7)[c(6,7,1,2,3,5,4)])
+  ethnic7 <- relevel(as.factor(ethnic7), ref="White")
+  return(ethnic7)
+}
+
